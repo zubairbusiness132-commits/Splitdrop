65ebef16823f34935584ef0f1b488089947b29f7
zubairbusiness132-commits <zubairbusiness132@gmail.com>
2026-08-03 20:35:48 +0530

    Add GitHub Actions deployment

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
-    base: './',
+    base: '/Splitdrop/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
